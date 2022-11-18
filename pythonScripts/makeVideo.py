4e26889fca053afdcb750ce28c36dfeed32d98e0
daniellehot <34506381+daniellehot@users.noreply.github.com>
2022-11-18 15:34:44 +0100

    Update makeVideo.py

--- pythonScripts/makeVideo.py
+++ pythonScripts/makeVideo.py
@@ -1,19 +1,23 @@
 import numpy as np
 import cv2 as cv
 import os 
+import random as rnd
 
 dataFolder = "../synthData/"
 
+
 def read_annotations(img_no):
-    #print(img_no)
     annotations = np.genfromtxt(gtFile, delimiter=",")
     annotations = annotations[:, :-1]
     annotations = annotations[annotations[:,0] == img_no]
-    #print(annotations)
     return annotations
 
-def draw_rectangles(img, annotations):
+
+def draw_rectangles(img, annotations, colors):
     for i in range(0,np.shape(annotations)[0]):
+        id = annotations[i, 1]
+        color = colors[ int(id -1) ]
+
         min_x = int(annotations[i, 2])
         min_y = int(annotations[i, 3])
         max_x = int(annotations[i, 2] + annotations[i, 4])
@@ -24,34 +28,28 @@ def draw_rectangles(img, annotations):
             print("Height overflow")
         center_x = int((min_x + max_x)/2)
         center_y = int((min_y + max_y)/2)
-        color = (rnd.randint(0,255), rnd.randint(0,255), rnd.randint(0,255))
-        #print("min " + str((min_x, min_y)))
-        #print("max " + str((max_x, max_y)))
         cv.rectangle(img, (min_x, min_y), (max_x, max_y), color, 2)
-        cv.putText(img, str(annotations[i, 1]), (center_x, center_y), cv.FONT_HERSHEY_DUPLEX, 1, color, 1)
-    #cv.imshow("test", img)
-    #cv.waitKey(0)
-    #return 0
+        cv.putText(img, str(id), (center_x, center_y), cv.FONT_HERSHEY_DUPLEX, 1, color, 1)
     return img
+
 
 def orderImages(imageFiles):
     for i in range(0, len(imageFiles)):
         imageFiles[i] = imageFiles[i].replace(".jpg", "")
         imageFiles[i] = int (imageFiles[i])
-
     imageFiles.sort()
-
-    for i in range(0, len(imageFiles)):
-        imageFiles[i] = str (imageFiles[i])
-        imageFiles[i] = imageFiles[i].zfill(6)
-        imageFiles[i] = imageFiles[i] + ".jpg"
-        
     return imageFiles        
 
 
-        
 def getRootFolders():
     return os.listdir(dataFolder)
+
+def generateColors(noOfColors):
+    colors = []
+    for i in range(int (noOfColors)):
+        colors.append((rnd.randint(0,255), rnd.randint(0,255), rnd.randint(0,255)))
+    return colors
+
 
 if __name__=="__main__":
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
@@ -62,14 +60,24 @@ if __name__=="__main__":
         for sequence in sequences:
             imgFolder = dataFolder + root + "/train/" + sequence + "/img1/"
             imageFiles = os.listdir(imgFolder)
-            imageFilesSorted = orderImages(imageFiles)
+            imagesSorted = orderImages(imageFiles)
             gtFile = dataFolder + root + "/train/" + sequence + "/gt/gt.txt"
             sequenceVideo = dataFolder + root + "/train/" + sequence + "/video.mp4"
             out = cv.VideoWriter(sequenceVideo, fourcc, 15.0, (960,544))
 
-            for image in imageFilesSorted:
-                img = cv.imread(imgFolder + image)
+            idColors = []
+            for img_no in imagesSorted:
+                img_name = str(img_no)
+                img_name = img_name.zfill(6) + ".jpg"
+                img = cv.imread(imgFolder+"/"+img_name)
+                #height, width, layers = img.shape
+                #size = (width,height)
+                #print(size)
+                annotations = read_annotations(img_no)
+                if (img_no == 1):
+                    idColors = generateColors(annotations[-1, 1])
+                img = draw_rectangles(img, annotations, idColors)
                 out.write(img)
-            
+
             out.release()
 
